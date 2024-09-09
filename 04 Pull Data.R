038d7ba0f8f121f19eff6e0e4cac2189ed6bdca8
kexite <denisostroushko1@gmail.com>
2024-09-09 11:19:58 -0500

    Making a push to aovid 60 day shut down rule

--- 04 Pull Data.R
+++ 04 Pull Data.R
@@ -565,8 +565,9 @@ cat("
   # some clearing: there is no easy way to convert EUR and other real currencies to 
   # USD that is easy. So, I am willing to discard these data 
   new_data2 <- bind_rows(populate_list)
-  new_data <- new_data2 %>% filter(!(symbol_to %in% c("EUR", "JPY", "AUD")))
-  
+  if("symbol_to" %in% colnames(new_data2)){
+    new_data <- new_data2 %>% filter(!(symbol_to %in% c("EUR", "JPY", "AUD")))
+  }
   
   if(nrow(new_data) != 0){#### old data has more column than the data that I have processed here 
     # combine the data first and then start doing the chained price conversions 
